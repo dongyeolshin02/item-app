d7ad2f7351af63c09730248536cd7ead0aaf80a4
dongyeolshin02 <eveing2@gmail.com>
2025-09-16 17:55:42 +0900

    33

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import { QueryClient } from '@tanstack/react-query';
 import './App.css'
+
+
+// React Query 클라이언트 생성
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 5 * 60 * 1000, // 5분
+    },
+  },
+});
+
 
 function App() {
 
